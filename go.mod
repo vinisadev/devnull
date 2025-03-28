1217fa45ebe46f99ee033e416872510f2bb38b54
Vincenzo Fehring <vinfehring@gmail.com>
2025-03-27 22:20:41 -0500

    Add database support

--- go.mod
+++ go.mod
@@ -2,10 +2,20 @@ module github.com/vinisadev/devnull
 
 go 1.24.1
 
-require github.com/bwmarrin/discordgo v0.27.1
+require (
+	github.com/bwmarrin/discordgo v0.27.1
+	gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.5
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -9,13 +9,51 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const (
 	deleteAfter = 2 * time.Minute
 )
 
+type Message struct {
+	gorm.Model
+	MessageID  string
+	ChannelID  string
+	Content    string
+	AuthorID   string
+	DeleteTime time.Time
+}
+
+var db *gorm.DB
+
+func initDB() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Auto migrate the Message model
+	err = db.AutoMigrate(&Message{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+}
+
 func main() {
+	// Initialize database
+	initDB()
+
 	// Get bot token from environment variable
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 	if token == "" {
@@ -49,6 +87,19 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore bot's own messages
 	if m.Author.ID == s.State.User.ID {
 		return
+	}
+
+	// Store message in database
+	msg := Message{
+		MessageID:  m.ID,
+		ChannelID:  m.ChannelID,
+		Content:    m.Content,
+		AuthorID:   m.Author.ID,
+		DeleteTime: time.Now().Add(deleteAfter),
+	}
+
+	if err := db.Create(&msg).Error; err != nil {
+		log.Printf("Error saving message to database: %v", err)
 	}
 
 	// Schedule message deletion
